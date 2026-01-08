d90ae34aa9e456c54e92c8e6b305fe17e8a8b8fe
Maria OPREA <maria.oprea1507@stud.acs.upb.ro>
2026-01-08 12:49:41 +0200

    partea de notif gata cu coding style

--- src/main/java/fornotifications/Observer.java
+++ src/main/java/fornotifications/Observer.java
@@ -1,8 +1,13 @@
-package forNotifications;
+package fornotifications;
 
 public interface Observer {
     // asta e nucleu la toata gestionarea de notif
     // asta spune cand trebuie sa adaugam o notif
+
+    /**
+     * metoda care declanseaza efectiv tot
+     * sistemul de notificari
+     */
     void update(String message);
 }
 
--- src/main/java/fornotifications/Subject.java
+++ src/main/java/fornotifications/Subject.java
@@ -1,9 +1,24 @@
-package forNotifications;
+package fornotifications;
 
 public interface Subject {
     // interfata care ma ajuta sa gestionez
     // develeoperii (ei sunt subject in cazul notif)
+
+    /**
+     * @param o
+     * functie pentru adaugare
+     */
     void addObserver(Observer o);
+
+    /**
+     *
+     * @param o
+     * functie pt stergere
+     */
     void removeObserver(Observer o);
+
+    /**
+     *  efectiv metoda de notificare
+     */
     void notifyObservers(String message);
 }
